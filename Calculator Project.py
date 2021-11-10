67a9fc6726dd929fe26c63117b07b9ae27f7df26
Deepika C <93979075+DeepikaC01@users.noreply.github.com>
2021-11-10 11:57:36 +0530

    Add files via upload

---
+++ Calculator Project.py
@@ -0,0 +1,36 @@
+# 1st Example - Addition
+
+# First_Number = int(input("Enter First Number: "))
+# Second_Number = int(input("Enter Second Number: "))
+# print(First_Number)
+# print(Second_Number)
+# Total_Sum = First_Number + Second_Number
+# print(Total_Sum)
+
+# 2nd Example - Addition with Decimal numbers
+
+# First_Number = float(input("Enter the 1st Number: "))
+# Second_Number = float(input("Enter the 2nd Number: "))
+# Total_Sum = (First_Number + Second_Number)
+# print(Total_Sum)
+
+# 3rd Example - Subtraction with Decimal numbers
+
+# First_Number = float(input("Enter the 1st Number: "))
+# Second_Number = float(input("Enter the 2nd Number: "))
+# Total_Sum = (First_Number - Second_Number)
+# print(Total_Sum)
+
+# 4th Example - Multiplication with Decimal numbers
+
+# First_Number = float(input("Enter the 1st Number: "))
+# Second_Number = float(input("Enter the 2nd Number: "))
+# Total_Sum = (First_Number * Second_Number)
+# print(Total_Sum)
+
+# 5th Example - Division with Decimal numbers
+
+# First_Number = float(input("Enter the 1st Number: "))
+# Second_Number = float(input("Enter the 2nd Number: "))
+# Total_Sum = (First_Number / Second_Number)
+# print(Total_Sum)
